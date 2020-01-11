337d3227798cc39fd230c79991f56bb10995bf08
nana <nykim@nykim.net>
2020-01-11 18:17:21 +0900

    리뷰 작성(수정) 화면 작성

--- js/ui.js
+++ js/ui.js
@@ -181,7 +181,7 @@ if (listCardContainer != null) {
 }
 
 
-/* TODO:지우기 */
+/* TODO:수정하기 */
 var selectStarIcon = document.querySelectorAll(".select-star-icon");
 selectStarIcon.forEach(function (el) {
   el.addEventListener("click", function (e) {
